6a55e5beb6b94940fa544e146f75f8c73b0628d6
Hasdai Pacheco <haxdai@gmail.com>
2017-12-13 17:30:05 -0600

    Implemented queryString search

--- src/main/java/mx/gob/cultura/search/api/v1/SearchEndPoint.java
+++ src/main/java/mx/gob/cultura/search/api/v1/SearchEndPoint.java
@@ -1,12 +1,18 @@
 package mx.gob.cultura.search.api.v1;
 
-import org.dizitart.no2.Cursor;
-import org.dizitart.no2.Document;
-import org.dizitart.no2.Nitrite;
-import org.dizitart.no2.NitriteCollection;
-import org.dizitart.no2.filters.Filters;
-import org.dizitart.no2.mapper.JacksonMapper;
-import org.dizitart.no2.mapper.NitriteMapper;
+import mx.gob.cultura.commons.Util;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -14,15 +20,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
+import java.io.IOException;
 
 @Path("/search")
 public class SearchEndPoint {
-    NitriteCollection coll;
-    Nitrite db;
+    RestHighLevelClient elastic;
 
     public SearchEndPoint() {
-        db = Util.getDB();
-        coll = db.getCollection("objects");
+        elastic = Util.DB.getElasticClient();
     }
 
     @GET
@@ -30,22 +35,68 @@ public class SearchEndPoint {
     public Response keywordSearch(@Context UriInfo context) {
         MultivaluedMap<String, String> params = context.getQueryParameters();
         String q = params.getFirst("q");
+        if (null == q) {
+            q = "";
+        }
         String id = params.getFirst("identifier");
 
-        JSONArray ret = new JSONArray();
-        Cursor cur;
+        JSONObject ret = new JSONObject();
 
-        if (null != id && !id.isEmpty()) {
-            cur = coll.find(Filters.elemMatch("identifier", Filters.eq("value", id)));
-        } else if (null != q && !q.isEmpty()) {
-            cur = coll.find(Filters.or(Filters.text("title", q), Filters.text("description", q)));
-        } else {
-            cur = coll.find();
-        }
+        //Create search request
+        SearchRequest sr = new SearchRequest("cultura");
 
-        for (Document doc : cur) {
-            NitriteMapper nitriteMapper = new JacksonMapper();
-            ret.put(new JSONObject(nitriteMapper.toJson(doc)));
+        //Create queryString query
+        SearchSourceBuilder ssb = new SearchSourceBuilder();
+        ssb.query(QueryBuilders.queryStringQuery(q));
+
+        //Build aggregations
+        TermsAggregationBuilder holdersAgg = AggregationBuilders.terms("holders")
+                .field("holder.raw");
+        TermsAggregationBuilder typesAgg = AggregationBuilders.terms("resourcetypes")
+                .field("resourcetype.raw");
+        DateHistogramAggregationBuilder datesAgg = AggregationBuilders.dateHistogram("dates")
+                .field("datecreated.value").dateHistogramInterval(DateHistogramInterval.YEAR);
+
+        ssb.aggregation(holdersAgg);
+        ssb.aggregation(typesAgg);
+        ssb.aggregation(datesAgg);
+
+        //Add source builder to request
+        sr.source(ssb);
+
+        try {
+            SearchResponse resp = elastic.search(sr);
+            if (resp.status().getStatus() == RestStatus.OK.getStatus()) {
+                ret.put("took", resp.getTook().toString());
+
+                //Get hits
+                SearchHits respHits = resp.getHits();
+                ret.put("count", respHits.getTotalHits());
+
+                SearchHit [] hits = respHits.getHits();
+                if (hits.length > 0) {
+                    JSONArray recs = new JSONArray();
+                    for (SearchHit hit : hits) {
+                        recs.put(new JSONObject(hit.getSourceAsString()));
+                    }
+                    ret.put("records", recs);
+
+
+                    /*Aggregations aggs = resp.getAggregations();
+                    if (aggs.asList().size() > 0) {
+                        Terms holders = aggs.get("holders");
+                        if (holders.getBuckets().size() > 0) {
+                            for (Terms.Bucket bucket : holders.getBuckets()) {
+                                System.out.println(bucket.getKeyAsString() + ":" + bucket.getDocCount());
+                            }
+                        }
+                    }*/
+                }
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
         }
 
         return Response.ok(ret.toString()).build();
